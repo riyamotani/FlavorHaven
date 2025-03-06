d2f84d655662b0b85e03d3a28bb16c7e76bd5ffa
riyamotani <riyamotani601@gmail.com>
2025-03-06 21:50:39 +0530

    url changes

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://flavorhaven.onrender.com",
     credentials:true
 }
 app.use(cors(corsOptions))
